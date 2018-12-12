50661e43636e78885260d5e8b3dae786861e3772
Sébastien Chèvre <seb.chevre@gmail.com>
2018-12-12 15:23:18 +0100

    add debit and credit operations

--- src/main/java/ch/hearc/clientsservice/application/api/web/ClientsController.java
+++ src/main/java/ch/hearc/clientsservice/application/api/web/ClientsController.java
@@ -5,12 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.hearc.clientsservice.application.service.ClientService;
 import ch.hearc.clientsservice.domaine.Client;
 import ch.hearc.clientsservice.domaine.CompteClient;
+import ch.hearc.clientsservice.domaine.command.CrediteClientCommand;
+import ch.hearc.clientsservice.domaine.command.DebiteClientCommand;
 
 @RestController
 @RequestMapping("/clients")
@@ -30,6 +35,26 @@ public class ClientsController {
 	public ResponseEntity<List<CompteClient>> getAllCompteClients () {
 		
 		return ResponseEntity.ok(clientService.getAllCompteClients());
+		
+	}
+	
+	@PostMapping("debit")
+	public ResponseEntity<CompteClient> debitCLient (
+			@RequestBody DebiteClientCommand debiteClientCommand ) {
+		
+		clientService.debite(debiteClientCommand);
+		
+		return ResponseEntity.ok(clientService.getCompteClientByIdentifiant(debiteClientCommand.getIdentifiantClient()));
+		
+	}
+	
+	@PostMapping("credit")
+	public ResponseEntity<CompteClient> creditCLient (
+			@RequestBody CrediteClientCommand crediteClientCommand ) {
+		
+		clientService.credite(crediteClientCommand);
+		
+		return ResponseEntity.ok(clientService.getCompteClientByIdentifiant(crediteClientCommand.getIdentifiantClient()));
 		
 	}
 }
